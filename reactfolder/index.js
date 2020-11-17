5866eae4f7c7c664e6409bb59404f00a185a18e9
venisha <vebhavsar@bestpeers.com>
2020-11-17 18:47:09 +0530

    text

---
+++ reactfolder/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-DOM'
+var name = "Wellcome"
+var element = <h1> Hello, Learner {name} </h1>
+
+ReactDOM.render(
+    element,document.getElementById('root')
+);
