2398642a2910791fcae78214c31d646e196417c7
venisha <vebhavsar@bestpeers.com>
2020-11-24 16:07:55 +0530

    X#	deleted:    reactfolder/myapp/node_modules/@babel/helper-hoist-variables/lib/index.js

--- reactfolder/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-DOM'
-var name = "Wellcome"
-var element = <h1> Hello, Learner {name} </h1>
-
-ReactDOM.render(
-    element,document.getElementById('root')
-);
